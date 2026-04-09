70f4867f4d1431944b5c9e5413c11215eacddfa4
Hassan-Nezar <s-hassan.hemida@zewailcity.edu.eg>
2026-04-10 00:17:21 +0200

    added program files folder, for files for read and write operations and data storage

---
+++ include/course.h
@@ -0,0 +1,14 @@
+#ifndef COURSE_H
+#define COURSE_H
+#include <string>
+using namespace std;
+
+class course {
+    private:
+        string c_name;
+        int c_id;
+        int c_hrs;
+          
+}
+
+#endif
